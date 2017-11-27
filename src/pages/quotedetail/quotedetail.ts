4f79fe98e5a7a1b0a44ec4a670542e644fa82e2c
Your Name <angular2guy@gmail.com>
2017-11-27 10:58:32 +0100

    quotedetails shows data

--- src/pages/quotedetail/quotedetail.ts
+++ src/pages/quotedetail/quotedetail.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { BitstampService } from '../../app/services/bitstamp.service';
+import { BitfinexService } from '../../app/services/bitfinex.service';
+import { QuoteBs } from '../../app/common/quoteBs';
+import { QuoteBf } from '../../app/common/quoteBf';
 
 /**
  * Generated class for the QuotedetailPage page.
@@ -14,13 +18,25 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'quotedetail.html',
 })
 export class QuotedetailPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  quoteBs = <QuoteBs> {};
+  quoteBf = <QuoteBf> {};
+  exchange: string;
+  
+  constructor(public navCtrl: NavController, public navParams: NavParams, private bitstampServ: BitstampService, private bitfinexServ: BitfinexService) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad QuotedetailPage');
-    console.log(this.navParams.get('exchange')+" "+this.navParams.get('currency'));
+    this.quoteBf = <QuoteBf> {};
+    this.quoteBs = <QuoteBs> {};
+    this.exchange = this.navParams.get('exchange');
+    if('bitstamp' === this.exchange) {
+        console.log("bitstamp"+" "+this.navParams.get('currency'));
+        this.bitstampServ.getCurrentQuote(this.navParams.get('currency')).subscribe(quote => this.quoteBs = quote);
+    } else if('bitfinex' === this.exchange) {
+        console.log("bitfinex"+" "+this.navParams.get('currency'));
+        this.bitfinexServ.getCurrentQuote(this.navParams.get('currency')).subscribe(quote => this.quoteBf = quote);
+    }
   }
 
 }
