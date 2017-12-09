edf7adb1de35dd5aec63fcd76337883e08c0090c
Your Name <angular2guy@gmail.com>
2017-12-09 17:06:31 +0100

    Settings

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -15,8 +15,10 @@ import * as CryptoJS from 'crypto-js';
   templateUrl: 'settings.html',
 })
 export class SettingsPage {
-  
+
+  bitstampId: string = null;
   bitstampKey: string = null;
+  bitfinexId: string = null;
   bitfinexKey: string = null;
   password: string = null;
     
@@ -27,19 +29,30 @@ export class SettingsPage {
     console.log('ionViewDidLoad SettingsPage');    
     let ud:Userdata = <Userdata> JSON.parse(localStorage.getItem(this.navParams.data.username));    
     let keys = ud.keys.filter(exch => exch.name === Exchange.BITSTAMP).map(exch => exch.token);
-    this.bitstampKey = keys.length > 0 ? keys[0] : null;
+    this.bitstampKey = keys.length > 0 ? "X" : null;    
+    keys = ud.keys.filter(exch => exch.name === Exchange.BITSTAMP).map(exch => exch.id);
+    this.bitstampId = keys.length > 0 ? "X" : null;
     keys = ud.keys.filter(exch => exch.name === Exchange.BITFINEX).map(exch => exch.token);
-    this.bitfinexKey = keys.length > 0 ? keys[0] : null;    
-  }
+    this.bitfinexKey = keys.length > 0 ? "X" : null;
+    keys = ud.keys.filter(exch => exch.name === Exchange.BITFINEX).map(exch => exch.id);
+    this.bitfinexId = keys.length > 0 ? "X" : null;
+  }     
   
   onSubmit() {
-    let ud:Userdata = <Userdata> JSON.parse(localStorage.getItem(this.navParams.data.username));    
-    let bfkey = CryptoJS.AES.encrypt(this.bitfinexKey, this.password+ud.salt.slice(0, 20)).toString();
-    let bskey = CryptoJS.AES.encrypt(this.bitstampKey, this.password+ud.salt.slice(0, 20)).toString();       
-    ud.keys = [];
-    ud.keys.push(new Exchange(Exchange.BITFINEX, bfkey));
-    ud.keys.push(new Exchange(Exchange.BITSTAMP, bskey));    
+    let ud:Userdata = <Userdata> JSON.parse(localStorage.getItem(this.navParams.data.username));
+    let bfid = CryptoJS.AES.encrypt(this.bitfinexId, this.password).toString();
+    let bfkey = CryptoJS.AES.encrypt(this.bitfinexKey, this.password).toString();
+    let bsid = CryptoJS.AES.encrypt(this.bitstampId, this.password).toString();
+    let bskey = CryptoJS.AES.encrypt(this.bitstampKey, this.password).toString();       
+    for(let i = 0;i<ud.keys.length;i++) {
+        if(ud.keys[i].name === Exchange.BITFINEX && this.bitfinexId !== "X" && this.bitfinexKey !== "X") {
+           ud.keys[i] = new Exchange(Exchange.BITFINEX, bfid, bfkey);
+        } else if(ud.keys[i].name === Exchange.BITSTAMP && this.bitstampId !== "X" && this.bitstampKey !== "X") {
+            ud.keys[i] = new Exchange(Exchange.BITSTAMP, bsid, bskey);
+        }
+    }        
     localStorage.setItem(this.navParams.data.username, JSON.stringify(ud));
-    
+    console.log("Settings saved.");
+//    console.log(CryptoJS.AES.decrypt(bsid, this.password).toString(CryptoJS.enc.Utf8));
   }
 }
